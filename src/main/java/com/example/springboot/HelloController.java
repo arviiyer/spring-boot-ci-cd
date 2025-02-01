6f0e1cfbdd6636a91b7b81baa986f66ebfb65c1f
Arvind Iyer <rbarvind04@gmail.com>
2025-02-01 18:10:59 -0500

    sample commit to test webhook integration

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Hello from Spring Boot!";
+		return "Hello from Spring Boot Version 2!!!";
 	}
 
 	@GetMapping("/DevOps")
